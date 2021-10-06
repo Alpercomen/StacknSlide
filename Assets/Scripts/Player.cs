fc9779d3737b04f8170da1728528ca1fda86f80f
Alpercomen <comenalper@gmail.com>
2021-10-06 05:00:59 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float xLimit = 1.0f;
+
+    private void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        ApplyForce();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void ApplyForce()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        if(Mathf.Abs(transform.localPosition.x) < xLimit)
+        {
+            transform.localPosition += new Vector3(xInput * speed, 0, 0);
+        }
+
+        if (xInput < 0 && transform.localPosition.x > 0)
+        {
+            transform.localPosition += new Vector3(xInput * speed, 0, 0);
+        }
+
+        if (xInput > 0 && transform.localPosition.x < 0)
+        {
+            transform.localPosition += new Vector3(xInput * speed, 0, 0);
+        }
+
+    }
+
+}
