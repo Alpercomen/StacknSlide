8c9688bd4ed3b29ba38af19682a6675a3982c649
Alpercomen <comenalper@gmail.com>
2021-10-08 05:10:45 +0300

    2.0 Release

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Dreamteck.Splines;
 
 
 public class Player : MonoBehaviour
 {
     public float horizontalSpeed = 1.0f;
     public float xLimit = 1.0f;
-    private float gForce = 0.0f;
     public float downwardForce = 0.1f;
     public float jumpForce = 2.0f;
+    public int ejectX = -125;
+    public int ejectY = 125;
+    public int ejectZ = 0;
+
+    private Rigidbody rb;
+    private float gForce = 0.0f;
     private bool onGround = true;
+    private GameObject parent;
+    private SplineFollower splineFollower;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        parent = GameObject.Find("Player");
+        splineFollower = parent.gameObject.GetComponent<SplineFollower>();
+    }
 
     void FixedUpdate()
     {
+        //Move left right
         ApplyForce();
     }
 
     private void Update()
     {
+        //Jump
         ApplyJump();
     }
 
@@ -28,16 +46,20 @@ public class Player : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         if (onGround) // If you want to be able to move in mid-air as well, simply remove this if condition.
         {
-            if(Mathf.Abs(transform.localPosition.x) < xLimit)
+
+            //Check whether you are inside the xLimit
+            if (Mathf.Abs(transform.localPosition.x) < xLimit)
             {
                 transform.localPosition += new Vector3(xInput * horizontalSpeed, 0, 0);
             }
 
+            //Help break free from the rightern limit
             if (xInput < 0 && transform.localPosition.x > 0)
             {
                 transform.localPosition += new Vector3(xInput * horizontalSpeed, 0, 0);
             }
 
+            //Help break free from the leftern limit
             if (xInput > 0 && transform.localPosition.x < 0)
             {
                 transform.localPosition += new Vector3(xInput * horizontalSpeed, 0, 0);
@@ -49,22 +71,52 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && onGround)
         {
+            //Apply the initial vertical vector
             transform.localPosition += new Vector3(0, jumpForce * Time.deltaTime, 0);
             onGround = false;
-        } 
+        }
 
-        if(!onGround)
+        if (!onGround)
         {
+            //Continue to apply it, but gradually increase it and start to apply it on the negative vertical axis.
             transform.localPosition += new Vector3(0, (jumpForce + gForce) * Time.deltaTime, 0);
             gForce -= downwardForce;
         }
 
-        if(transform.localPosition.y <= 0 && !onGround)
+        if (transform.localPosition.y <= 0 && !onGround)
         {
+            //Fix the spot of the player on exact y = 0 spot
             transform.localPosition = new Vector3(transform.localPosition.x, 0, 0);
             onGround = true;
-            gForce = 0.0f;
+            gForce = 0.0f; //Reset the gForce
         }
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Finish"))
+        {
+            Eject();
+        }
+    }
+
+    public void Eject()
+    {
+        
+        
+        float followerSpeed = splineFollower.followSpeed; //Store Spline Follower speed
+        parent.GetComponent<Rigidbody>().velocity = new Vector3(ejectX, ejectY, ejectZ) * followerSpeed; //Apply vector on the given direction.
+        Vector3 speed = parent.GetComponent<Rigidbody>().velocity; //Store it in speed Vector
+
+        parent.transform.DetachChildren(); //Detach children, so that rigidbody can work as intended.
+
+        if(this.gameObject.GetComponent<Rigidbody>() == null)
+        {
+            this.gameObject.AddComponent<Rigidbody>(); //Add rigidbody
+            this.gameObject.GetComponent<Rigidbody>().AddForce(speed.x, speed.y, speed.z); //Add force to our Rigidbody
+        }
+
+    }
 }
+
--- Assets/Scripts/PlayerCatch.cs
+++ Assets/Scripts/PlayerCatch.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Dreamteck.Splines;
 using UnityEngine;
 
 public class PlayerCatch : MonoBehaviour
 {
-    private static int peopleCount = 0;
+
+    [SerializeField] 
+    public Transform stackPosition;
+    public float stackGap = 0.5f;
+    public float speedIncrease = 0.5f;
+    public float jumpDecrease = 2.0f;
+    public float fovIncrease = 2;
+    [HideInInspector]
+    public int peopleCount = 0;
+
     private Vector3 newPos;
-    [SerializeField] Transform stackPosition;
+    private SplineFollower spline;
+    private Camera camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        peopleCount = 0;
+        spline = GameObject.Find("Player").GetComponent<SplineFollower>();
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -24,27 +37,49 @@ public class PlayerCatch : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Pickup"))
         {
-
             Transform parent = other.transform;
             Transform child = parent.Find("Dummy").gameObject.transform;
-
-
+            
+            //Set the heads position on the stack
+            newPos.y = 0f;
+            newPos.x = 0; 
+            newPos.z -= stackGap; 
             stackPosition.localPosition = newPos;
 
+            //Increase our speed, increase our field of view, increase people count
+            spline.followSpeed += speedIncrease;
+            camera.fieldOfView += fovIncrease;
+            peopleCount++;
+
+            //Make the "Dummy" inside our parent object a child of this stack position.
             child.SetParent(stackPosition);
             child.localPosition = new Vector3(newPos.x, -1.5f, newPos.z);
             child.localRotation = Quaternion.identity;
             child.localScale = new Vector3(1.25f, 1.25f, 1.25f);
 
-
-
             newPos = stackPosition.localPosition;
-            newPos.y = 0f;
-            newPos.x = 0;
-            newPos.z -= 0.5f;
 
+            //Disable the parent position
+            other.gameObject.SetActive(false);
+        }
 
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            GameObject head = GameObject.Find("Head");
             
+            if(head.transform.childCount == 0)
+            {
+                //Implement death mechanic here
+                Debug.Log("You are dead");
+            } 
+            else
+            {
+                //Destroy the first child, reduce our speed, reduce field of view, reduce people count
+                Destroy(head.transform.GetChild(0).gameObject);
+                spline.followSpeed -= speedIncrease;
+                camera.fieldOfView -= fovIncrease;
+                peopleCount--;
+            }
         }
     }
 }
